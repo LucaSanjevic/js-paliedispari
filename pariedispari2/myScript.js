cf35d9d012a00f17962c9cf30aed7a3570e4ddf3
LucaSanjevic <luca-sanjevic@hotmail.it>
2021-04-15 09:54:21 +0200

    aggiustato

--- pariedispari2/myScript.js
+++ pariedispari2/myScript.js
@@ -1,12 +1,12 @@
 var pariodispari = prompt("Pari o dispari?");
-var inserisciNumero = prompt("Inserisci un numero");
+var inserisciNumero = prompt("Inserisci un numero da 1 a 5");
 
 getSomma(pariodispari, inserisciNumero)
 // funzione che genera un numero random da 1 a 5
 function getSomma(_pariodispari, _inserisciNumero) {
-    var num = 5;
+    var num = Math.floor(Math.random()*5);
     var somma = num + _inserisciNumero;
-    if(somma % 2 == 0) {
+    if(somma % 2 == 1) {
         if(_pariodispari == "Pari") {
             return console.log("Hai vinto, il numero è pari");
                                  }
